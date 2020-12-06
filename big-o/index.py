2ce056a077d49b3d35f796351d5be12e81a35e28
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2020-12-06 22:58:46 +0700

    reformat folder

--- big-o/index.py
+++
@@ -1,9 +0,0 @@
-# Example
-# -- O(n) --
-#   So the time complexity of this function depends on the number parameter of it. 
-#   func(num = 1) will be lower than func(num = 100) 100 times, 
-#   and func(num=n) will be greater than func(num=1) n times
-
-def multiplyBy10(num):
-    return num * 10
-
