5c5ed3ee5f02f580bdd9eec1813f8749ac40662f
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2020-12-08 02:44:02 +0700

    quick sort finished

--- python/sorts/quick_sort.py
+++ python/sorts/quick_sort.py
@@ -2,15 +2,31 @@ def quick_sort(arr: list):
     length = len(arr)
     if length <= 1:
         return
+
     i = -1
     j = 0
     pivot = arr[length - 1]
+    
     while j < length - 1:
         if arr[j] <= pivot:
             i += 1
             arr[j], arr[i] = arr[i], arr[j]
         j += 1
 
-    left = arr[:i]
-    right = arr[i + 1:]
-    print(left,right)
+    pivot_i = i + 1
+
+    arr[pivot_i], arr[j] = arr[j], arr[pivot_i]
+
+    left = arr[:pivot_i]
+    right = arr[pivot_i + 1:]
+
+    quick_sort(left)
+    quick_sort(right)
+
+    length_l = len(left)
+    length_r = len(right)
+
+    for k in range(length_l):
+        arr[k] = left[k]
+    for k in range(length_r):
+        arr[length_l + k + 1] = right[k]
