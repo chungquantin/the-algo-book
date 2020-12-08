f395effdc1d48e4970b263080f8c2a3855c89f61
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2020-12-08 21:21:19 +0700

    Update test.py

--- python/sorts/test.py
+++ python/sorts/test.py
@@ -26,7 +26,7 @@ algorithms = [
     optimized_bubble_sort,
 ]
 # Create sample
-s = sample(5000, 0, 1000000)
+s = sample(5000, -1000, 1000)
 # Choose analysis mode
 sorts_analysis(s, algorithms)
 sorts_table_analysis(s, algorithms)
