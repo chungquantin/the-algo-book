dffd82c27a19c650fb724b469ff9d8309da52261
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2020-12-13 18:52:40 +0700

    Update min_coins.py

--- python/dynamic_programming/min_coins.py
+++ python/dynamic_programming/min_coins.py
@@ -2,13 +2,13 @@ import math
 
 
 def min_coins(_sum, coins: list):
-    dp = [math.inf] * _sum
+    dp = [math.inf] * (_sum + 1)
     dp[0] = 0
-    for s in range(_sum):
+    for s in range(1, _sum + 1):
         for coin in coins:
             if s >= coin:
                 dp[s] = min(dp[s - coin] + 1, dp[s])
-    return dp[_sum - 1]
+    return dp[_sum]
 
 
-print(min_coins(12, [1, 2, 5]))
+print(min_coins(17, [2, 3, 5, 8]))
